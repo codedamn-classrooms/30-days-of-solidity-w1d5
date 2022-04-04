0cac2e6884cf14adc7b6addd3714ca061e60d8e0
kshitijofficial <52281964+kshitijofficial@users.noreply.github.com>
2022-04-04 17:13:08 +0530

    Update day6.sol

--- contracts/day6.sol
+++ contracts/day6.sol
@@ -2,11 +2,5 @@
 pragma solidity >=0.5.0 <0.9.0;
 
 contract day6 {
-    function average(
-        int256 a,
-        int256 b,
-        int256 c
-    ) public pure returns (int256) {
-        return (a + b + c) / 3;
-    }
+   
 }
